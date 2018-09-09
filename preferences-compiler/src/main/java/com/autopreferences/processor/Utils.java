2b2efd9be2b950e62c12644efe8780fc30c64860
Anisonyan Edward <4238662+Starksoft@users.noreply.github.com>
2018-09-09 21:25:59 +0300

    Initial code drop

---
+++ preferences-compiler/src/main/java/com/autopreferences/processor/Utils.java
@@ -0,0 +1,20 @@
+package com.autopreferences.processor;
+
+import java.util.List;
+
+import javax.lang.model.element.Element;
+
+public final class Utils {
+
+	private Utils() {
+		throw new UnsupportedOperationException();
+	}
+
+	static String camelCase(String in) {
+		return in.substring(0, 1).toUpperCase() + in.substring(1, in.length());
+	}
+
+	static boolean isNullOrEmpty(List<? extends Element> enclosedElements) {
+		return enclosedElements == null || enclosedElements.isEmpty();
+	}
+}
